52735ac5bd56a784534d7dc20f64fe0150489a47
rubenverhoef <ruben_verhoef@hotmail.com>
2018-09-27 21:15:57 +0200

    fixing issues with mysql 8.0 connect error

--- overlay/root/mysql.sh
+++ overlay/root/mysql.sh
@@ -5,4 +5,5 @@
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $1;"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$2'@'%' IDENTIFIED BY '$3';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $1.* TO '$2'@'%' WITH GRANT OPTION;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "ALTER USER '$2'@'localhost' IDENTIFIED WITH mysql_native_password BY '$3';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
