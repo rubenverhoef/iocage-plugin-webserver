0bb49d521f25beb9cd965d53c982221f3aef0037
rubenverhoef <ruben_verhoef@hotmail.com>
2018-09-27 20:45:04 +0200

    Mysql 8.0 user creation fix

--- overlay/root/mysql.sh
+++ overlay/root/mysql.sh
@@ -3,5 +3,6 @@
 . $(dirname $0)/webserver_config.sh
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $1;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $1.* TO '$2'@'%' IDENTIFIED BY '$3';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$2'@'%' IDENTIFIED BY '$3';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $1.* TO '$2'@'%' WITH GRANT OPTION;"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
